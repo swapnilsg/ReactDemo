f79cbd7877ebb2256db42715856f7c42ef6b900f
swapnil gadekar <swapnilsg5@gmail.com>
2016-06-28 00:19:10 +0530

    second

--- js/app.js
+++ js/app.js
@@ -13,10 +13,15 @@ console.log("hello");
     });
 
     var TodoList = React.createClass({
+        removeItem:function(dItem){
+        console.log('remove:'+dItem);
+        this.props.onDelete(dItem);
+    },
       render: function() {
+          var self=this;
       var createItem = function(itemText) {
         return (
-            <TodoListItem>{itemText}</TodoListItem>
+            <TodoListItem rmItem={self.removeItem} >{itemText}</TodoListItem>
           );
       };
       return <ul className="list-group">{this.props.items.map(createItem)}</ul>;
@@ -24,9 +29,19 @@ console.log("hello");
     });
 
     var TodoListItem = React.createClass({
+        delete:function(e){
+            e.preventDefault();
+            console.log(this.props.children);
+            this.props.rmItem(this.props.children);
+        },
       render: function(){
         return (
-          <li className="{list-group-item}">{this.props.children}</li>
+          <li className="{list-group-item}">{this.props.children}
+            <form onSubmit={this.delete}>
+                    <button>&times;</button>
+                </form>
+            
+            </li>
         );
       }
     });
@@ -67,11 +82,26 @@ console.log("hello");
         items: allItems
       });
       },
+      deleteItems:function(dItem){
+        console.log('delete:');
+          var index;
+        this.state.items.filter(function(_item, _index){
+            if (_item == dItem){
+                index = _index;
+            }
+        });
+          
+        this.state.items.splice(index,1);
+          this.setState({
+        items: this.state.items
+      });
+          
+      },
       render: function() {
       return (
         <div className="container">
         <TodoBanner/>
-        <TodoList items={this.state.items}/>
+        <TodoList items={this.state.items} onDelete={this.deleteItems}/>
         <TodoForm onFormSubmit={this.updateItems}/>
         </div>
       );
